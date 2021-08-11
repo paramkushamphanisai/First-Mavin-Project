d303e5cb5e78315e0c6fcd7a5d832ec06671734f
PVVPhaniSai <paramkushamphanisai@gmail.com>
2021-08-11 21:28:27 +0530

    Adding Code For Tomcat

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -2,9 +2,12 @@ package com.example;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.matchers.GreaterThan;
+import org.mockito.internal.matchers.LessThan;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.containsString;
 
@@ -34,13 +37,6 @@ public class TestGreeter {
     @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
-    
-    @Test
-  public void greetShouldIncludeGreetingMessageToFriends() {
-    String someone = "World, Thanks you making me Happy to Friends";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
